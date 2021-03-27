8c89d7150cda27140c33c80d2b6a80ddf669586b
Edgar Guerrero <edgarguerrero37@gmail.com>
2021-03-26 21:38:42 -0500

    Jugo en Unity

---
+++ Assets/OcultarYEliminar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OcultarYEliminar : MonoBehaviour
+{
+    public Text contador;
+    public GameObject vida;
+    public int inicio;
+
+    void Start()
+    {
+        inicio = 3;
+        contador = GameObject.Find("Puntaje").GetComponent<Text>();
+        contador.text = "Vidas: " + inicio.ToString();
+        /* vida = GameObject.Find("heart");
+        Destroy(vida); */
+    }
+
+    public void eliminar()
+    {
+        vida = GameObject.Find("vidas");
+        /* Destroy(vida); */
+        vida.SetActive(false);
+        inicio--;
+        contador.text = inicio.ToString();
+    }
+}
